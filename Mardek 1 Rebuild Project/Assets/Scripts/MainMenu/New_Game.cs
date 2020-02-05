80b77997d016e75b2edf9904092a125ecbe07a57
Lasermancer <Lasermancer@users.noreply.github.com>
2020-02-05 01:03:24 +0100

    .

---
+++ Mardek 1 Rebuild Project/Assets/Scripts/MainMenu/New_Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class New_Game : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GameFile.x = 0.08f;
+        GameFile.y = -0.4f;
+        GameFile.party_members = new string[] { "Mardek_Hero", "Deugan_Hero", "", ""};
+        SceneManager.LoadScene("DL_Area1");
+    }
+}
