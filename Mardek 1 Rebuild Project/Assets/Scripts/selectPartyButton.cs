726d0578e4a2058cfd9815763669ba922445fdb7
DreamToucan <mikkola.tuomas@gmail.com>
2020-07-14 18:02:18 +0300

    Previous entermenu drastically changed.

---
+++ Mardek 1 Rebuild Project/Assets/Scripts/selectPartyButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectPartyButton : MonoBehaviour
+{
+    private GameObject partyButtonGo;
+    private Button button;
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        partyButtonGo = GameObject.Find("PartyButton");
+        button = partyButtonGo.GetComponent<Button>();
+        button.Select();
+    }*/
+
+    /*private void Awake()
+    {
+        partyButtonGo = GameObject.Find("PartyButton");
+        button = partyButtonGo.GetComponent<Button>();
+        button.Select();
+    }*/
+
+    private void OnEnable()
+    {
+        partyButtonGo = GameObject.Find("PartyButton");
+        button = partyButtonGo.GetComponent<Button>();
+        button.Select();
+    }
+}
