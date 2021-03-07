9c6a222d197cf3968ed75c9700b6662acd4670b4
GeorgeManeta <georgemaneta@gmail.com>
2021-03-07 11:59:09 -0300

    transition + waypoint WIP

---
+++ MARDEK-Unity-Revival-Project/Assets/Scripts/JRPG/Transition/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JRPG {
+    [RequireComponent(typeof(GridObject))]
+    public class Waypoint : MonoBehaviour
+    {
+        [SerializeField] WaypointEnum thisWaypoint = null;
+
+        private void Start()
+        {
+            if (thisWaypoint)
+                if (thisWaypoint == Transition.usedWaypoint)
+                    Debug.Log(transform.position);
+        }
+    }    
+}
