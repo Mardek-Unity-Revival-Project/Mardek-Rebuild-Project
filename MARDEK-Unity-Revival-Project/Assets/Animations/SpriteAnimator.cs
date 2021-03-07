25f627c4b1517d25e35990aa594a412b664000f2
GeorgeManeta <georgemaneta@gmail.com>
2021-03-07 07:19:53 -0300

    forget about the experimental reskining unity feature

---
+++ MARDEK-Unity-Revival-Project/Assets/Animations/SpriteAnimator.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] Dictionary<string, Sprite> keyValuePairs = new Dictionary<string, Sprite>();
+}
