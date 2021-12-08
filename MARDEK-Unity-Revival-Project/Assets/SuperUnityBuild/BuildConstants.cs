2b2f7384ab0a349a2f37b194652eca7f5aca6425
George Maneta <georgemaneta@gmail.com>
2021-12-08 06:19:51 -0300

    Configure SuperBuild parameters

---
+++ MARDEK-Unity-Revival-Project/Assets/SuperUnityBuild/BuildConstants.cs
@@ -0,0 +1,37 @@
+// This file is auto-generated. Do not modify or move this file.
+
+public static class BuildConstants
+{
+    public enum ReleaseType
+    {
+        None,
+        Release,
+    }
+
+    public enum Platform
+    {
+        None,
+        PC,
+        WebGL,
+    }
+
+    public enum Architecture
+    {
+        None,
+        Windows_x64,
+        WebGL,
+    }
+
+    public enum Distribution
+    {
+        None,
+    }
+
+    public static readonly System.DateTime buildDate = new System.DateTime(637745405463830246);
+    public const string version = "0.0.2";
+    public const ReleaseType releaseType = ReleaseType.Release;
+    public const Platform platform = Platform.WebGL;
+    public const Architecture architecture = Architecture.WebGL;
+    public const Distribution distribution = Distribution.None;
+}
+
