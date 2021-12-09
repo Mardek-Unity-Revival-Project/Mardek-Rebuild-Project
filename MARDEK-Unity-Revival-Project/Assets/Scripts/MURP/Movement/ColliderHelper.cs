7dcc79debeca64d7ff6826c9ac63d0a3946a7d2a
George Maneta <georgemaneta@gmail.com>
2021-12-09 18:43:57 -0300

    Fix bugs in compilation

--- MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/Movement/ColliderHelper.cs
+++ MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/Movement/ColliderHelper.cs
@@ -6,7 +6,7 @@ namespace MURP.Movement
     [RequireComponent(typeof(Collider2D))]
     public class ColliderHelper : MonoBehaviour
     {
-        new Collider2D collider = null;
+        Collider2D _collider = null;
         ContactFilter2D filter = default;
         List<Collider2D> results = new List<Collider2D>();
         Collider2D[] colliders = new Collider2D[8];
@@ -18,8 +18,8 @@ namespace MURP.Movement
 
         void InitializeFields()
         {
-            collider = GetComponent<Collider2D>();
-            if (collider)
+            _collider = GetComponent<Collider2D>();
+            if (_collider)
             {
                 filter.useLayerMask = true;
                 LayerMask mask = Physics2D.GetLayerCollisionMask(gameObject.layer);
@@ -29,17 +29,17 @@ namespace MURP.Movement
 
         public void OffsetCollider(Vector2 offset)
         {
-            if (collider)
-                collider.offset = offset;
+            if (_collider)
+                _collider.offset = offset;
         }
 
         public List<Collider2D> Overlaping()
         {
             results.Clear();
-            if (collider == null)
+            if (_collider == null)
                 return results;
             System.Array.Clear(colliders, 0, colliders.Length);
-            collider.OverlapCollider(filter, colliders);
+            _collider.OverlapCollider(filter, colliders);
             foreach (Collider2D c in colliders)
                 if (c != null && c.isActiveAndEnabled)
                     results.Add(c);
