4abc1a734b9b1320454b7e1e7af91d19536b500e
George Maneta <georgemaneta@gmail.com>
2021-12-22 05:41:06 -0300

    Implement transcript parsing to dialogue objects

--- MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/Dialogue System/CharacterLines.cs
+++ MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/Dialogue System/CharacterLines.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
-using MURP.CharacterSystem;
+using CharacterInfo = MURP.CharacterSystem.CharacterInfo;
 
 namespace MURP.DialogueSystem
 {
     [System.Serializable]
     public class CharacterLines
     {
-        [field: SerializeField] public CharacterSystem.CharacterInfo Character { get; private set; }
+        [field: SerializeField] public CharacterInfo Character { get; private set; }
         [field: TextArea(0,5)]
         [field: SerializeField] public List<string> Lines { get; private set; }
+
+        public CharacterLines(CharacterInfo info, List<string> lines)
+        {
+            Character = info;
+            Lines = lines;
+        }
     }
 }
