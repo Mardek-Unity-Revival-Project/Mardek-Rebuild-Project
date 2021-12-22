69de1ae6d93a9b6a68ea191f309ed7acd4ca30b8
George Maneta <georgemaneta@gmail.com>
2021-12-22 08:41:13 -0300

    Improve scene load after battle. Create cutscenes at last area of dragon lair.

---
+++ MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/Battle System/StartBattleCommand.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MURP.EventSystem;
+using UnityEngine.SceneManagement;
+using MURP.SaveSystem;
+
+namespace MURP.BattleSystem
+{
+    public class StartBattleCommand : CommandBase
+    {
+        [SerializeField] EncounterSet encounter = null;
+        [SerializeField] SceneReference battleScene = default;
+
+        public override void Trigger()
+        {
+            BattleManager.encounter = encounter;
+            AddressableMonoBehaviour.SaveOnTransition();
+            SceneManager.LoadScene(battleScene);
+        }
+    }
+}
