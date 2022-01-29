d0079f70e95af3ef7d737d789e858e660648b6b8
knokko <knokogator@hotmail.com>
2022-01-29 14:37:07 +0100

    Update the current value and max value texts in ConditionBar

--- MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/UI/InGameMenu/Party/ConditionBar.cs
+++ MARDEK-Unity-Revival-Project/Assets/Scripts/MURP/UI/InGameMenu/Party/ConditionBar.cs
@@ -18,6 +18,9 @@ namespace MURP.UI
             if (this.backgroundColors.Count != this.textColors.Count) throw new System.ArgumentException("Must have same number of text colors as background colors");
             if (this.backgroundColors.Count < 1) throw new System.ArgumentException("Must have at least 1 background color");
 
+            this.currentValue.text = currentValue.ToString();
+            this.maxValue.text = maxValue.ToString();
+            
             float currentFraction = (float) currentValue / (float) maxValue;
             int colorIndex = (int) (currentFraction * (this.backgroundColors.Count - 1) + 0.5f);
 
